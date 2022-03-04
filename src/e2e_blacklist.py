c301b5f5f9e711d0df7503a9d4f61df7a1675c82
Dan Putnam <danputnam1@gmail.com>
2022-03-04 02:20:13 -0800

    Rewrote e2e tests by hand

--- src/e2e_blacklist.py
+++ src/e2e_blacklist.py
@@ -1,15 +1,18 @@
 import subprocess
+from time import sleep
+import unittest
+import requests
 
 class e2eTests(unittest.TestCase):
 
+  #tests that local nginx server is up on the tester image and returns pong
   def test_nginx_up(self):
-    "local nginx server is up on the tester image and should return 'pong'"
     r = requests.get("http://localhost:8086")
     self.assertEqual(r.status_code, 200)
     self.assertEqual(r.text, "pong")
 
-  def test_packetwatch_allow(self):
-    "packetwatch should not stop a slow portscan (tweak portscan settings before beginning test)"
+  #tests that packetwatch doesn't stop a slow portscan
+  def test_slow_portscan(self):
     requests.get("http://localhost:8086", timeout=1)
     sleep(1.5)
     requests.get("http://localhost:8087", timeout=1)
@@ -20,8 +23,8 @@ class e2eTests(unittest.TestCase):
     self.assertEqual(r.status_code, 200)
     self.assertEqual(r.text, "pong")
 
+  #tests that packetwatch doesn't stop someone from hammering a single port even if it's faster than the threshold
   def test_packetwatch_singleport_allow(self):
-    "packetwatch should not stop someone hammering a single port"
     requests.get("http://localhost:8086", timeout=1)
     requests.get("http://localhost:8086", timeout=1)
     requests.get("http://localhost:8086", timeout=1)
@@ -29,6 +32,7 @@ class e2eTests(unittest.TestCase):
     self.assertEqual(r.status_code, 200)
     self.assertEqual(r.text, "pong")
 
+  #tests that packetwatch correctly stops a portscan
   def test_packetwatch_deny(self):
     "packetwatch should stop a portscan"
     requests.get("http://localhost:8086", timeout=1)
@@ -43,11 +47,39 @@ class e2eTests(unittest.TestCase):
 
 
 #turn nginx on.  Yes, it is a race condition, I know, I know.
+print("Building nginx server for packetwatch to protect")
+print("Make sure no traffic is coming over localhost during these tests, or it will interfere with packetwatch's port scanning!")
 subprocess.call('service nginx start', shell=True)
 sleep(5)
 
 tester=e2eTests()
-tester.test_nginx_up()
-tester.test_packetwatch_allow()
-tester.test_packetwatch_singleport_allow()
-tester.test_packetwatch_deny()
+
+try:
+  print("Checking to make sure nginx is up!")
+  tester.test_nginx_up()
+  print("Success!")
+except AssertionError:
+  print("nginx is not up!  This will skew future tests.")
+
+try:
+  print("Performing a slow portscan, packetwatch should allow this.  Be sure PW_PORTSCAN_TIME_THRESHOLD is 1 second or less.")
+  tester.test_slow_portscan()
+  print("Success!")
+except AssertionError:
+  print("Couldn't reach nginx server!  It may be down, or packetwatch may not be expiring calls correctly.")
+
+try:
+  print("Performing hammer test, faster than packetwatch's threshold but with fewer ports than PW_PORTSCAN_PORT_THRESHOLD.")
+  tester.test_packetwatch_singleport_allow()
+  print("Success!")
+except AssertionError:
+  print("Couldn't reach nginx server!  It may be down, or packetwatch may not be honoring port thresholds correctly.")
+
+try:
+  print("Performing packetwatch filter test.  Packetwatch should flag me as a portscanner and prevent nginx access.")
+  tester.test_packetwatch_deny()
+  print("Success, I'm blocked!!")
+except AssertionError:
+  print("Was able to reach nginx server!  That's bad, we should have been blocked.  is packetwatch running?")
+
+print("Thus ends our tests.  Restart packetwatch to try again.")
