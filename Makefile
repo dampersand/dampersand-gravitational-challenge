052c46e2008be4b18fad6590d1c740163886d743
Dan Putnam <dampersand@users.noreply.github.com>
2022-03-04 19:06:18 -0800

    Feature/pass through envs (#10)
    * Add environment passthrough to docker-compose, include usage in the makefile.
    
    * Wow just found out that python doesn't treat getenv values '' as None, so they can't be defaulted.  My disappointment is immeasurable and my day is ruined.
    
    * man that's super embarrassing to submit.
    
    * Add make stop

--- Makefile
+++ Makefile
@@ -1,12 +1,35 @@
-.PHONY: help build-app build-test build-all exec-app exec-test run run-log unit-test e2e-white e2e-black clean
+.PHONY: help build-app build-test build-all exec-app exec-test run run-log unit-test e2e-white e2e-black stop clean
 .DEFAULT_GOAL:= help
 
+#Options
+#?# PW_IFDEV
+#?# 	network interface name that packetwatch will monitor (ex: eth0, eth2) (default: lo)
+#?# PW_PORTSCAN_TIME_THRESHOLD	
+#?# 	time threshold (seconds) for scanning (default: 60)
+#?# PW_PORTSCAN_PORT_THRESHOLD
+#?# 	number of ports threshold for scanning (default: 3)
+#?# 	If you scan PW_PORTSCAN_PORT_THRESHOLD ports within PW_PORTSCAN_TIME_THRESHOLD, you are flagged as a port-scanner
+#?# PW_PROMETHEUS_PORT	
+#?# 	port to serve prometheus metrics (default: 9090)
+#?# PW_WHITELIST_SELF
+#?# 	prevent IPs attached to the monitored interface from being labeled as scanners (default: True)
+
 help:       ## Print this help
-	@echo 'Usage: make <target>'
+	@echo 'Usage: make <target> [OPTION=value]'
 	@echo 'Setup or run the Packetwatch tool'
 	@echo
 	@echo 'Targets:'
 	@fgrep -h "##" $(MAKEFILE_LIST) | fgrep -v fgrep | sed -e 's/\\$$//' | sed -e 's/##//'
+	@echo
+	@echo 'Options:'
+	@fgrep -h "#?#" $(MAKEFILE_LIST) | fgrep -v fgrep | sed -e 's/\\$$//' | sed -e 's/#?#//'
+	@echo
+	@echo 'Example Usages:'
+	@echo 'run packetwatch in the background on eth0.  Do not blacklist connections from IP addresses associated with eth0'
+	@echo '  make run PW_IFDEV=eth0 '
+	@echo
+	@echo 'run packetwatch in the foreground on eth0.  Blacklist connections regardless of their association with eth0.  Anyone who scans 3 (default) separate ports in 10 seconds is a scanner.'
+	@echo '  make run-log PW_IFDEV=eth0 PW_WHITELIST_SELF=False PW_PORTSCAN_TIME_THRESHOLD=10'
 
 build-app:  ## Build packetwatch
 	@docker-compose build packetwatch
@@ -24,7 +47,7 @@ exec-app:   ## Step into the packetwatch container for debug
 exec-test:  ## Step into the tester container for debug
 	@docker-compose run --rm tester /bin/bash
 
-run:        ## Run packetwatch in the background
+run:        ## Run packetwatch in the background.
 	@docker-compose run --rm --detach packetwatch
 
 run-log:    ## Run packetwatch, attach and watch logs
@@ -38,6 +61,9 @@ e2e-black:  ## Run e2e tests without whitelisting self (test portscan detection)
 
 e2e-white:  ## Run e2e tests but whitelist self (test whitelist capability, but not portscan detection)
 	@docker-compose -f docker-compose.yml -f docker-compose.e2ewhite.yml up
+
+stop:				## Stop any instances of packetwatch or tester.
+	@docker-compose down
 
 clean:      ## Remove all instances of packetwatch and tester from your machine (including images)
 	@docker-compose down --rmi all
--- src/main.py
+++ src/main.py
@@ -5,37 +5,73 @@ from pwHelpers import getSelfIPs, ipHRTo32, outputColumns
 import logging
 from sys import stdout
 
+#######
+#SETUP
+#######
+
 #constants
-PORTSCAN_TIME_THRESHOLD = int(getenv('PW_PORTSCAN_TIME_THRESHOLD', 60))
-PORTSCAN_PORT_THRESHOLD = int(getenv('PW_PORTSCAN_PORT_THRESHOLD', 3)) #Hit PORTSCAN_PORT_THRESHOLD ports within PORTSCAN_TIME_THRESHOLD and you're now a scanner
-PROMETHEUS_PORT         = int(getenv('PW_PROMETHEUS_PORT', 9090))
-IFDEV                   = getenv('PW_IFDEV', "lo")
-WHITELIST_SELF          = getenv('PW_WHITELIST_SELF', 'True').lower() in ('true', '1') #sneaky trick, yes?
+#Not using os.getenv's default setting because that doesn't detect empty strings.  Booooooooooo.
+ENV = {
+  "PORTSCAN_TIME_THRESHOLD" : getenv('PW_PORTSCAN_TIME_THRESHOLD'),
+  "PORTSCAN_PORT_THRESHOLD" : getenv('PW_PORTSCAN_PORT_THRESHOLD'), #Hit PORTSCAN_PORT_THRESHOLD ports within PORTSCAN_TIME_THRESHOLD and you're now a scanner
+  "PROMETHEUS_PORT"         : getenv('PW_PROMETHEUS_PORT'),
+  "IFDEV"                   : getenv('PW_IFDEV'),
+  "WHITELIST_SELF"          : getenv('PW_WHITELIST_SELF')
+}
+
+ENVDEFAULT = {
+  "PORTSCAN_TIME_THRESHOLD" : 60,
+  "PORTSCAN_PORT_THRESHOLD" : 3,
+  "PROMETHEUS_PORT"         : 9090,
+  "IFDEV"                   : "lo",
+  "WHITELIST_SELF"          : True
+}
+
+#Python's treatment of environment variables is trash.  Because '' is "falsey" but not falsey enough to count as "None",
+#we can't use os.getenv's 'default' argument, so we gotta set our default values and types manually
+for key in ENV:
+  if ENV[key] == '':
+    ENV[key] = ENVDEFAULT[key]
+
+#Booleans are special cases.  We need to cast them from strings regardless.
+if type(ENV['WHITELIST_SELF']) != bool:
+  ENV['WHITELIST_SELF'] = ENV['WHITELIST_SELF'].lower() in ('true', '1') #sneaky trick, yes?
+
+#Now typecast everything.  yea double assignment we doin' it
+for key in ENV:
+  ENV[key] = type(ENVDEFAULT[key])(ENV[key])
+
+
 
 #log setup
 logging.basicConfig(stream = stdout, level=logging.INFO, format="%(message)s") #doesn't get simpler than this
 
 #prometheus setup
-start_http_server(PROMETHEUS_PORT)
+start_http_server(ENV["PROMETHEUS_PORT"])
 
 #instantiate bpf interface
-interface = bpfBuddy("bpf/packetwatch.c", PORTSCAN_PORT_THRESHOLD, PORTSCAN_TIME_THRESHOLD, IFDEV)
+interface = bpfBuddy("bpf/packetwatch.c", ENV["PORTSCAN_PORT_THRESHOLD"], ENV["PORTSCAN_TIME_THRESHOLD"], ENV["IFDEV"])
+
+
+#######
+#START
+#######
 
 #Greet the user and put up pretty columns
 logging.info("Welcome to Packetwatch!")
-logging.info("We will be logging all incoming connections on %s" % IFDEV)
-logging.info("Any source IPs that hit %s different ports within %s second(s) will have their future connection attempts dropped!" % (PORTSCAN_PORT_THRESHOLD, PORTSCAN_TIME_THRESHOLD))
+logging.info("We will be logging all incoming connections on %s" % ENV["IFDEV"])
+logging.info("Any source IPs that hit %s different ports within %s second(s) will have their future connection attempts dropped!" % (ENV["PORTSCAN_PORT_THRESHOLD"], ENV["PORTSCAN_TIME_THRESHOLD"]))
 whitelistInfo = False
-if WHITELIST_SELF:
-  whitelistInfo = ("", IFDEV, "!")
+if ENV["WHITELIST_SELF"]:
+  whitelistInfo = ("", ENV["IFDEV"], "!")
 else:
-  whitelistInfo = (" NOT", IFDEV, ", so play nice!")
+  whitelistInfo = (" NOT", ENV["IFDEV"], ", so play nice!")
 logging.info("We will%s whitelist any incoming connections originating from our own IPs on %s%s" % whitelistInfo)
 outputColumns("TIME", "IP", "PORT", "MESSAGE")
 
 #whitelist ourselves!
-if WHITELIST_SELF:
-  for ip in getSelfIPs(IFDEV):
+if ENV["WHITELIST_SELF"]:
+  for ip in getSelfIPs(ENV["IFDEV"]):
     interface.forceWhitelist(ipHRTo32(ip))
 
 #And get to work!
